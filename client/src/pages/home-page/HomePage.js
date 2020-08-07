e2aa00cb69e1f089757ff98285d38b505fd72b64
unknown <dzevad.del@gmail.com>
2020-08-07 10:59:40 +0200

    test 2

--- client/src/pages/home-page/HomePage.js
+++ client/src/pages/home-page/HomePage.js
@@ -27,13 +27,7 @@ const HomePage = () => {
   console.log('homepage/list', list);
 
   useEffect(() => {
-    const fetchAPI = async () => {
-      const response = await axios.get('/test');
-      console.log('message from server/', response);
-    };
-
-    fetchAPI();
-    // dispatch(fetchProductsFromDb());
+    dispatch(fetchProductsFromDb());
     window.scrollTo(0, 0);
   }, [dispatch]);
 

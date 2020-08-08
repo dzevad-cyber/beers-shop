afd4d2280e6fbd301a81e0b7e70f5508a00b63c7
unknown <dzevad.del@gmail.com>
2020-08-08 12:46:03 +0200

    test 3

--- client/src/pages/home-page/HomePage.js
+++ client/src/pages/home-page/HomePage.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
 
 import styles from './HomePage.module.scss';
 
